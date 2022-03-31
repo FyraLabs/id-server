7c7408195395f323473a87b51d67bcc929315f42
Lleyton Gray <lleyton@innatical.com>
2022-03-30 21:30:58 -0700

    Return if email is verified

--- routes/user/me.go
+++ routes/user/me.go
@@ -9,8 +9,9 @@ func GetMe(c *fiber.Ctx) error {
 	user := c.Locals("user").(*ent.User)
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"id":    user.ID.String(),
-		"email": user.Email,
-		"name":  user.Name,
+		"id":            user.ID.String(),
+		"email":         user.Email,
+		"name":          user.Name,
+		"emailVerified": user.EmailVerified,
 	})
 }
