92f6076dabb9e429dd31c50b5009e4c31e8ad027
Lleyton Gray <lea@fyralabs.com>
2022-04-29 00:02:29 -0700

    something about avatars go fuck yourself

--- routes/user/me.go
+++ routes/user/me.go
@@ -8,11 +8,23 @@ import (
 func GetMe(c *fiber.Ctx) error {
 	user := c.Locals("user").(*ent.User)
 
+	if user == nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "unauthorized",
+		})
+	}
+
+	avatarURL := "https://hashvatar.vercel.app/" + user.ID.String() + "/stagger"
+
+	if user.AvatarURL != nil {
+		avatarURL = *user.AvatarURL
+	}
+
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"id":            user.ID.String(),
 		"email":         user.Email,
 		"name":          user.Name,
 		"emailVerified": user.EmailVerified,
-		"avatarURL":     user.AvatarURL,
+		"avatarURL":     avatarURL,
 	})
 }
